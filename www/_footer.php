2c6b6b1defaa5bc282ebf8e2541352037ec98ca9
Alkis Georgopoulos <alkisg@gmail.com>
2020-07-13 19:56:56 +0300

    Update against latest just-the-docs

--- www/_footer.php
+++ www/_footer.php
@@ -1,4 +1,4 @@
-															
+
 </DIV> <!-- this div is inside the row with the rest of header and logs text -->
 </div>
 </div>
@@ -16,9 +16,9 @@ else {
 }
 ?>
 
-		<hr> 
+		<hr>
 		<footer role="contentinfo" style="padding: 0px 10px;">
-			<p class="text-small text-grey-dk-000 mb-0">Copyright &copy; 2000-2019 the <a href="https://github.com/ltsp/ltsp/graphs/contributors">LTSP developers</a></p>
+			<p class="text-small text-grey-dk-000 mb-0">Copyright &copy; 2000-2020 the <a href="https://github.com/ltsp/ltsp/graphs/contributors">LTSP developers</a></p>
 		</footer>
 		</div>
 	</div>
